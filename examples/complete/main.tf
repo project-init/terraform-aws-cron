e549a8a18179ec1f2c604298de2c27657ac040b7
Derek Rushing <20804720+drush211@users.noreply.github.com>
2026-01-02 15:26:34 -0600

    Update task.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,5 +1,31 @@
+resource "aws_scheduler_schedule_group" "group" {
+  name = "group"
+}
+
 module "cron" {
   source = "project-init/worker-service/aws"
   # Project Init recommends pinning every module to a specific version
   # version = "vX.X.X"
+
+  environment  = "staging"
+  service_name = "service"
+  # group_name   = aws_scheduler_schedule_group.group.name
+  cron_name = "cron"
+
+  schedule_expression = "rate(30 minutes)"
+
+  # ECS Cluster/Service
+  ecs_cluster_arn = "ecs_arn"
+  subnets         = ["subnet1"]
+  security_groups = []
+
+  # ECS Task
+  image                 = "123456789.dkr.ecr.us-east-1.amazonaws.com/image:v0.0.1"
+  environment_variables = []
+
+  cpu    = 256
+  memory = 512
+
+  # Load Balancer/Routing
+  vpc_id = "vpc_id"
 }
--- iam.tf
+++ iam.tf
@@ -10,16 +10,21 @@ resource "aws_iam_role" "cron" {
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
+    effect  = "Allow"
 
     principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
+      type = "Service"
+      identifiers = [
+        "scheduler.amazonaws.com",
+        "ecs-tasks.amazonaws.com"
+      ]
     }
   }
 }
 
-data "aws_iam_policy_document" "ecs_exec_policy" {
+data "aws_iam_policy_document" "policy" {
   version = "2012-10-17"
+  # ECS Exec Permissions
   statement {
     effect = "Allow"
     actions = [
@@ -36,19 +41,35 @@ data "aws_iam_policy_document" "ecs_exec_policy" {
       "*"
     ]
   }
+
+  # DLQ Permissions
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:SendMessage"
+    ]
+    resources = [
+      aws_sqs_queue.deadletter_queue.arn
+    ]
+  }
 }
 
-resource "aws_iam_policy" "ecs_exec_policy" {
-  name   = "${var.service_name}-ecs-exec-policy"
-  policy = data.aws_iam_policy_document.ecs_exec_policy.json
+resource "aws_iam_policy" "policy" {
+  name   = "${var.service_name}-cron-${var.cron_name}-policy"
+  policy = data.aws_iam_policy_document.policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "service_policy" {
-  role       = aws_iam_role.service.name
+resource "aws_iam_role_policy_attachment" "service_policy_events" {
+  role       = aws_iam_role.cron.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
+}
+
+resource "aws_iam_role_policy_attachment" "service_policy_ecs" {
+  role       = aws_iam_role.cron.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_exec_policy" {
-  role       = aws_iam_role.service.name
-  policy_arn = aws_iam_policy.ecs_exec_policy.arn
+resource "aws_iam_role_policy_attachment" "policy" {
+  role       = aws_iam_role.cron.name
+  policy_arn = aws_iam_policy.policy.arn
 }
